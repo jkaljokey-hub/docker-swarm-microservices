0591862190d1eac04109a95bd57f29ac25ee294e
jkaljokey-hub <jkaljokey@gmail.com>
2026-07-07 18:49:27 +0000

    fix ssh key

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ async function start() {
     console.log("✅ Connected to PostgreSQL Database");
 
     await redis.connect();
-    console.log("✅ Connecsted to Redis Database");
+    console.log("✅ Connected to Redis Database");
 
     await db.query(`
       CREATE TABLE IF NOT EXISTS posts(
